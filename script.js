ada7541c4f7347733279c048d59034a8a77beee3
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2024-01-29 21:59:24 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,76 @@
+const API = `https://dummyjson.com/comments`;
+
+const chatForm = $("#chatForm");
+const chatFormBtn = $("#chatFormBtn");
+const textMessage = $("#chatForm :input");
+const chatMain = $("#chatMain");
+const partnerTyping = $("#partnerTyping");
+let comments = [];
+
+const scrollToBottom = (el) => el.scrollTop(el[0].scrollHeight);
+
+const getRandomId = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const createMessage = (time, sender, text) => {
+   return `<div class="message ${sender || ``}">
+      <div class="message-block message-block--${sender || ``}">
+         <p class="message-block__text" id="messageText">
+            ${
+               sender
+                  ? text
+                  : comments[`${getRandomId(1, comments.length)}`].body
+            }
+         </p>
+         <span class="message-block__time"
+            >${time}</span
+         >
+      </div>
+   </div>`;
+};
+
+const generateAnswer = () => {
+   partnerTyping.text("Typing...");
+
+   setTimeout(() => {
+      const time = new Date().toLocaleTimeString([], {
+         hour: "2-digit",
+         minute: "2-digit",
+      });
+      partnerTyping.text("");
+      chatMain.append(createMessage(time));
+      scrollToBottom(chatMain);
+   }, 2000);
+};
+
+const sendUserMessage = (e) => {
+   e.preventDefault();
+   if (textMessage.val()) {
+      const sender = `user`;
+      const text = textMessage.val();
+      const time = new Date().toLocaleTimeString([], {
+         hour: "2-digit",
+         minute: "2-digit",
+      });
+
+      chatMain.append(createMessage(time, sender, text));
+      chatForm[0].reset();
+      scrollToBottom(chatMain);
+
+      generateAnswer();
+   }
+};
+
+chatFormBtn.on("click", sendUserMessage);
+
+$(document).ready(() => {
+   scrollToBottom(chatMain);
+   textMessage[0].focus();
+});
+
+//get comments
+(async () => {
+   let phrases = await fetch(API).then((data) => data.json());
+   // console.log(phrases.comments);
+
+   comments = phrases.comments;
+})();
